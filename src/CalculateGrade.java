fda862b3422d9ca11c0ac3a198c789a531bdd670
Roman2543 <ghostzrome2543@gmail.com>
2021-09-01 15:09:33 +0700

    edit structurer Calculate class

--- src/CalculateGrade.java
+++ src/CalculateGrade.java
@@ -1,4 +1,19 @@
 
 public class CalculateGrade {
+	private int score;
 
+	public CalculateGrade(int score) {
+		
+	}
+	public int getScore() {
+		return 0;
+	}
+	public void setScore(int score) {
+		
+	}
+	public char calculate() {
+		return 'A';
+	}
+	
+	
 }
